502ef26cb419ef41d3c2efc804c08ef7ad53da92
ky66 <ky66@duke.edu>
2018-12-04 22:05:07 -0500

    g

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -39,23 +39,23 @@ public class PercolationBFS extends PercolationDFSFast{
             col = p%size;
             
 
-				if (!isFull(row-1, col) || isOpen(row-1, col)) {
+				if (inBounds(row-1,col) && !isFull(row-1, col) && isOpen(row-1, col)) {
 					myGrid[row-1][col] = FULL; 
 					qp.add(myGrid[row-1][col]);
 				}
 				
-				if (!isFull(row+1, col) || isOpen(row+1, col)) {
+				if (inBounds(row+1,col) && !isFull(row+1, col) && isOpen(row+1, col)) {
 					myGrid[row+1][col] = FULL; 
 					qp.add(myGrid[row+1][col]);
 				}
 				
 				
-				if (!isFull(row, col-1) || isOpen(row, col-1)) {
+				if (inBounds(row,col-1) && !isFull(row, col-1) && isOpen(row, col-1)) {
 					myGrid[row][col-1] = FULL; 
 					qp.add(myGrid[row][col-1]);
 				}
 			
-				if (!isFull(row, col+1) || isOpen(row, col+1)) {
+				if (inBounds(row,col+1) && !isFull(row, col+1) && isOpen(row, col+1)) {
 					myGrid[row][col+1] = FULL; 
 					qp.add(myGrid[row][col+1]);;
 				}
